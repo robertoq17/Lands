e02e1636a8a99e65db7a60c14710e9fbc6acc85e
robertoq17 <robertoq17@gmail.com>
2018-12-04 16:55:52 -0400

    Lands

---
+++ Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.Infrastructure
+{
+    using ViewModels;
+
+    class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+
+
+}
---
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.ViewModels
+{
+    class MainViewModel
+    {
+
+        #region ViewModel
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+
+        public LandsViewModel Lands { get; set; }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            instance = this;
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+
+        #region Singleton
+        private static MainViewModel instance;
+
+        public static MainViewModel GetInstance()
+        {
+            if(instance == null)
+            {
+                return new MainViewModel();
+            }
+
+            return instance;
+        }
+        #endregion
+    }
+}
