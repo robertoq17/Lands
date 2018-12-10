b9b4b622c6da0392fc7e798666df5ade7dc13e97
robertoq17 <robertoq17@gmail.com>
2018-12-10 10:55:02 -0400

    borrado de innecesariedades

--- Lands/Lands/Infrastructure/InstanceLocator.cs
+++ Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Lands.Infrastructure
+﻿namespace Lands.Infrastructure
 {
     using ViewModels;
 
--- Lands/Lands/ViewModels/LandsViewModel.cs
+++ Lands/Lands/ViewModels/LandsViewModel.cs
@@ -3,7 +3,6 @@
     using GalaSoft.MvvmLight.Command;
     using Models;
     using Services;
-    using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Linq;
